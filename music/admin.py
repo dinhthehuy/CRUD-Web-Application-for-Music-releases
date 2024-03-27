ae90ab1289fa08f0e89fe9fb29c32ba5079442a4
dinhthehuy <dinhthehuy51@gmail.com>
2024-03-27 09:59:34 +0100

    add database model to admin page

--- music/admin.py
+++ music/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from music.models import Album, Artist, LoggedAlbum, AlbumTag
+from music.models import Album, Artist, Log, AlbumTag
 
 # Register your models here.
 admin.site.register(Album)
 admin.site.register(Artist)
-admin.site.register(LoggedAlbum)
+admin.site.register(Log)
 admin.site.register(AlbumTag)
