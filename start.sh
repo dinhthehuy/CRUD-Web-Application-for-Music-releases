66ee289a99d81ef78dd89e431c9982c35ac5f29a
dinhthehuy <dinhthehuy51@gmail.com>
2024-04-04 18:29:28 +0200

    separate into staging and production environments

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 source .venv/Scripts/activate
 cd /code || exit
-mkdir staticfiles || echo "staticfiles folder already existed"
 echo "-----------Apply migration--------- "
 python manage.py makemigrations
 python manage.py migrate
-echo "-----------Collect static files-----------"
-python manage.py collectstatic
 
-echo "-----------Run gunicorn--------- "
-gunicorn logNewAlbum.wsgi:application --bind 0.0.0.0:8000
