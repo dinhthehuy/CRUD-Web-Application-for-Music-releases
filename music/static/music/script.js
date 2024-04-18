830f481e85b6a7e67e77d08e3e62eec9b05f0e91
dinhthehuy <dinhthehuy51@gmail.com>
2024-04-18 16:13:52 +0200

    rename function, hideListItems now shows image url

--- music/static/music/script.js
+++ music/static/music/script.js
@@ -1,21 +1,35 @@
 function hideListItems() {
-    const button = document.querySelector('#EditView')
-    let listItems = document.querySelectorAll('li');
+    const button = document.querySelector('#edit-button')
+    let viewCells = document.querySelectorAll('.view-cell')
+    let editCells = document.querySelectorAll('.edit-cell')
+    const imageUrlCell = document.querySelector('#image-url-cell')
+    const updateButton = document.querySelector('#update-info-button')
     if(button.value === 'Edit') {
-        listItems.forEach(function(item) {
-            item.style.display = 'none';
-        });
         button.value = 'View'
-    } else {
-        listItems.forEach(function(item) {
-            item.style.display = 'block';
-        });
-        button.value = 'Edit'
-    }
+        editCells.forEach(function (item) {
+            item.style.display = 'table-row'
+        })
 
+        viewCells.forEach(function (item) {
+            item.style.display = 'none'
+        })
+        imageUrlCell.style.display = 'table-row'
+        updateButton.style.display = 'block'
+    } else {
+        button.value = 'Edit'
+        editCells.forEach(function (item) {
+            item.style.display = 'none'
+        })
+
+        viewCells.forEach(function (item) {
+            item.style.display = 'table-cell'
+        })
+        imageUrlCell.style.display = 'none'
+        updateButton.style.display = 'none'
+    }
 }
 
-function updateImage() {
+function updateSearchResultImage() {
     const imageUrl = document.getElementById('image_url').value
     const background = document.getElementById('background-image')
     const coverArt = document.getElementById('found-album-cover-art')
