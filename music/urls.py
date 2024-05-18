3328a8df2e12875f359397677ad44edb82a22c43
dinhthehuy <dinhthehuy51@gmail.com>
2024-05-18 14:33:15 +0200

    refactor: add endpoint url

--- music/urls.py
+++ music/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('delete/<log_id>/', views.delete_log, name='delete'),
     path('update/<album_id>/', views.update_album, name='update'),
     path('tag/<path:album_tag>', views.get_albums_by_tag, name='tag'),
-    path('log', views.filter_log_by_year, name='filter_log'),
+    path('log', views.filter_log, name='filter_log'),
     path('log/<page_number>/', views.get_log_pagination, name='log'),
     path('save-note', views.save_note, name='save_note'),
 ]
